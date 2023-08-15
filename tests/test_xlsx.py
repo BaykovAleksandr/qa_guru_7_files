1f4abee188341ce7d23449a07a4b42072b8813d7
BaykovAleksandr <baykov-1988@mail.ru>
2023-08-15 09:02:00 +0300

    Исправил etst_xlsx

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -8,10 +8,11 @@ from conftest import RESOURCES_DIR
 def test_xlsx_file():
     workbook = load_workbook(os.path.join(RESOURCES_DIR, 'file_example_XLSX_50.xlsx'))
     sheet = workbook.active
-    print(sheet.cell(row=3, column=2).value)
-    assert sheet.cell(row=3, column=2).value == 'Mara'
+    print(sheet.cell(row=3, column=3).value)
+    assert sheet.cell(row=3, column=3).value == 'Hashimoto'
 
     os.remove(os.path.join(RESOURCES_DIR, 'file_example_XLSX_50.xlsx'))
+
 
 
 
